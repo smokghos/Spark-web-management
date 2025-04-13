d3ea1d31d32819742619f5a302419697ca2e3d8a
smokghos <linforest0067@gmail.com>
2025-04-13 16:11:09 +0800

    0.2

--- src/main/java/com/spark/controller/UploadController.java
+++ src/main/java/com/spark/controller/UploadController.java
@@ -2,37 +2,35 @@ package com.spark.controller;
 
 
 import com.spark.pojo.Result;
+import com.spark.utils.AliyunOSSOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.File;
+
 import java.util.UUID;
 
 @Slf4j
 @RestController
 public class UploadController {
 
-    private static final String UPLOAD_DIR = "G:\\open\\Spark-web-management\\src\\main\\resources\\images\\";
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
 
     @PostMapping("/upload")
-    public Result upload(String name , Integer age, MultipartFile file) throws Exception {
-        log.info("上传文件：{}, {}, {}", name, age, file);
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("上传文件：{}", file);
         if (!file.isEmpty()) {
             // 生成唯一文件名
             String originalFilename = file.getOriginalFilename();
             String extName = originalFilename.substring(originalFilename.lastIndexOf("."));
             String uniqueFileName = UUID.randomUUID().toString().replace("-", "") + extName;
-            // 拼接完整的文件路径
-            File targetFile = new File(UPLOAD_DIR + uniqueFileName);
-
-            // 如果目标目录不存在，则创建它
-            if (!targetFile.getParentFile().exists()) {
-                targetFile.getParentFile().mkdirs();
-            }
-            // 保存文件
-            file.transferTo(targetFile);
+            // 上传文件
+            String url = aliyunOSSOperator.upload(file.getBytes(), uniqueFileName);
+            return Result.success(url);
         }
-        return Result.success();
+        return Result.error("上传失败");
     }
+
 }
