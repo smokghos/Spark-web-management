5f0314df63464b0ad6ed580ed20a34a145733093
smokghos <linforest0067@gmail.com>
2025-04-14 10:18:16 +0800

    0.9.1

--- src/main/java/com/spark/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spark/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.spark.exception;
 
 import com.spark.pojo.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +17,15 @@ public class GlobalExceptionHandler {
         log.error("操作失败,错误原因是： " + e);
         //捕获到异常之后，响应一个标准的Result
         return Result.error("对不起,操作失败,请联系管理员");
+    }
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(DuplicateKeyException exception){
+        log.error("程序出错啦 " + exception);
+        String message = exception.getMessage();
+        int i = message.indexOf("Duplicate entry");
+        String errMessage = message.substring(i);
+        String[] arr = errMessage.split(" ");
+        return Result.error(arr[2] + "已存在");
     }
 
 }
